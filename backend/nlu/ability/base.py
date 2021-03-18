6bded5c8ab0336928fc268a4ab0f3a2b254a78ff
hanbing <beatmight@gmail.com>
2021-03-18 17:34:01 +0800

    develop: 增加了内置识别能力的接口

---
+++ backend/nlu/ability/base.py
@@ -0,0 +1,2 @@
+class _BaseAbility(object):
+    pass
---
+++ backend/nlu/ability/ner.py
@@ -0,0 +1,13 @@
+from backend.nlu.ability.base import _BaseAbility
+
+
+class PersonAbility(_BaseAbility):
+    pass
+
+
+class LocationAbility(_BaseAbility):
+    pass
+
+
+class OrganizationAbility(_BaseAbility):
+    pass
