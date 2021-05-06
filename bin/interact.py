0987d5fd9cd17b2e234c9b4ca1268c873e54eba6
hanbing <beatmight@gmail.com>
2021-05-06 12:08:35 +0800

    feature: 在交互测试脚本增加了打印机器人理解详细信息的功能

--- bin/interact.py
+++ bin/interact.py
@@ -1,15 +1,16 @@
 """
 交互测试机器人对话情况
 """
+from utils.define import MODEL_TYPE_NLU, MODEL_TYPE_DIALOGUE
+import backend.manager as manager
 import os
 import json
+from pprint import pprint as print
 from config import global_config
 
 # 不提前加载机器人，只加载需要测试的机器人
 global_config["_delay_loading_robot"] = True
 
-import backend.manager as manager
-from utils.define import MODEL_TYPE_NLU, MODEL_TYPE_DIALOGUE
 
 # 加载参数
 cwd = os.path.abspath(os.path.dirname(__file__))
@@ -23,11 +24,14 @@ data = manager.session_create(params["robot_code"], "user1", params["params"])
 sessionId = data["sessionId"]
 print("机器人说：{}".format(data["says"]))
 
+data = None
 while True:
     user_says = input("用户说：")
     if user_says == "info":
         print(manager.agents[
             params["robot_code"]].user_store[sessionId]._latest_msg())
+    elif user_says == "verbose":
+        print(data)
     else:
         data = manager.session_reply(params["robot_code"], sessionId,
                                      user_says)
