6b7725fb59d82e7fe8064e75c0e21e6b393debe3
hanbing <beatmight@gmail.com>
2021-07-15 13:27:00 +0800

    feature: faq现在增加了回答阈值和推荐阈值作为参数

--- app/say.py
+++ app/say.py
@@ -27,10 +27,17 @@ class ReplySessionHandler(_BaseHandler):
         params = kwargs.get("params", {})
         recommend_num = kwargs.get("recommend_num", 5)
         perspective = kwargs.get("perspective", FAQ_DEFAULT_PERSPECTIVE)
+        rcm_threshold = kwargs.get("rcm_threshold", -1)
+        ans_threshold = kwargs.get("ans_threshold", -1)
 
         faq_params = {
             "recommend_num": recommend_num,
             "perspective": perspective
         }
+
+        if rcm_threshold > 0:
+            faq_params["rcm_threshold"] = rcm_threshold
+        if ans_threshold > 0:
+            faq_params["ans_threshold"] = ans_threshold
 
         return session_reply(robot_code, session_id, user_says, user_code, params, faq_params)
