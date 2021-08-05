e2e12d661c11976c36a075a6452df37c5b2d2607
BrightXiaoHan <beatmight@gmail.com>
2021-08-05 11:10:56 +0800

    fix: 修复了service_interact脚本中的残留的建立连接的请求

--- bin/service_interact.py
+++ bin/service_interact.py
@@ -7,14 +7,18 @@ serve_port = global_config["serve_port"]
 
 cwd = os.path.abspath(os.path.dirname(__file__))
 params = json.load(open(os.path.join(cwd, "params.json")))
+session_id = "service_interact"
 
+says = input("用户说：")
 data = {
     "robotId": params["robot_code"],
     "userCode": "user1",
-    "params": params["params"]
+    "params": params["params"],
+    "userSays": says,
+    "sessionId": session_id
 }
 response = post_rpc(
-    "http://127.0.0.1:{}/api/v1/session/create".format(serve_port),
+    "http://127.0.0.1:{}/api/v1/session/reply".format(serve_port),
     data
 )
 print(response)
@@ -24,7 +28,7 @@ while True:
     data = {
         "robotId": params["robot_code"],
         "userCode": "user1",
-        "sessionId": response["data"]["sessionId"],
+        "sessionId": session_id,
         "userSays": says
     }
     response = post_rpc(
