8e46b25b99d9e256373a69aa36bbfd3b4a84cfbd
BrightXiaoHan <beatmight@gmail.com>
2021-08-17 13:45:11 +0800

    feature: 增加了车牌号，电话号码中文数字转阿拉伯数字的功能

--- backend/dialogue/nodes/builtin/regx.py
+++ backend/dialogue/nodes/builtin/regx.py
@@ -1,20 +1,26 @@
 import re
+import cn2an
 
 internal_regx_ability = {
-    "@sys.plates": ["[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领]{1}[A-Z查叉插]{1}-?[A-Z0-9查叉插]{4}[A-Z0-9挂学警港澳查叉插]{1}[A-Z0-9查叉插]?"],
+    "@sys.plates": [
+        "[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领]{1}[A-Z查叉插]{1}-?[A-Z0-9查叉插]{4}[A-Z0-9挂学警港澳查叉插]{1}[A-Z0-9查叉插]?"
+    ],
     "@sys.phone": ["(?:(?:\+|00)86)?1[3-9]\d{9}"],
-    "@sys.tel": ["(?:(?:\d{3}-)?\d{8}|^(?:\d{4}-)?\d{7,8})(?:-\d+)?"]
+    "@sys.tel": ["(?:(?:\d{3}-)?\d{8}|^(?:\d{4}-)?\d{7,8})(?:-\d+)?"],
 }
 
-internal_regx_ability = {key: [re.compile(item) for item in value]
-                         for key, value in internal_regx_ability.items()}
+internal_regx_ability = {
+    key: [re.compile(item) for item in value]
+    for key, value in internal_regx_ability.items()
+}
 
 
 def builtin_regx(msg):
     # 解析内置能力正则
+    text = cn2an.transform(msg.text, "cn2an")
     for k, vs in internal_regx_ability.items():
         for v in vs:
-            regx_values = v.findall(msg.text)
+            regx_values = v.findall(text)
             if regx_values:
                 msg.add_entities(k, regx_values)
     return
