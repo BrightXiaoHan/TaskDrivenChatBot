733d1fd4018216b72a258f41d7c635395d37d656
BrightXiaoHan <beatmight@gmail.com>
2023-01-31 15:09:59 +0800

    update

--- xiaoyu_cli/service_interact.py
+++ xiaoyu_cli/service_interact.py
@@ -1,8 +1,8 @@
 import json
 import os
 
-from config import global_config
-from utils.funcs import post_rpc
+from xiaoyu.config import global_config
+from xiaoyu.utils.funcs import post_rpc
 
 serve_port = global_config["serve_port"]
 
@@ -29,7 +29,5 @@ while True:
         "sessionId": session_id,
         "userSays": says,
     }
-    response = post_rpc(
-        "http://127.0.0.1:{}/api/v1/session/reply".format(serve_port), data
-    )
+    response = post_rpc("http://127.0.0.1:{}/api/v1/session/reply".format(serve_port), data)
     print(response)
